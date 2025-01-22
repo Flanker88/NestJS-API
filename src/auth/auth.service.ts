d5f3932d75ad649b84bf3a8860acfee373761d2e
Flanker88 <trungdong88vp@gmail.com>
2025-01-22 16:57:23 +0700

    upload project first

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,74 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { User, Note } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable({})
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+  async register(authDTO: AuthDTO) {
+    const hashedPassword = await argon.hash(authDTO.password);
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: authDTO.email,
+          hashedPassword: hashedPassword,
+          firstName: '',
+          lastName: '',
+        },
+
+        select: {
+          id: true,
+          email: true,
+          createAt: true,
+        },
+      });
+      return await this.convertToJwtString(user.id, user.email);
+    } catch (error) {
+      if (error.code == 'P2002') {
+        throw new ForbiddenException('Error in credentials');
+      }
+      return {
+        error,
+      };
+    }
+  }
+  async login(authDTO: AuthDTO) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email: authDTO.email },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+
+    const passwordMatched = await argon.verify(
+      user.hashedPassword,
+      authDTO.password,
+    );
+
+    if (!passwordMatched) {
+      throw new ForbiddenException('Incorrect password');
+    }
+
+    delete user.hashedPassword;
+    return await this.convertToJwtString(user.id, user.email);
+  }
+  async convertToJwtString(userId: number, email: string): Promise<string> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    return this.jwtService.signAsync(payload, {
+      expiresIn: '10m',
+      secret: this.configService.get('JWT_SECRET'),
+    });
+  }
+}
